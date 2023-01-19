9b76aa40ecc1790f9f64dd6711b492f2177b8ea3
sara-gaballa <96298154+sara-gaballa@users.noreply.github.com>
2023-01-19 21:47:41 +0200

    back

---
+++ backend/src/main/java/com/example/ProducerConsumer/ProducerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProducerConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerConsumerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/ProducerConsumer/ProducerConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProducerConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
