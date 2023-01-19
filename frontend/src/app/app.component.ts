47332dea1eda3d503e9b7a37f752dacddabfe8a4
sara-gaballa <96298154+sara-gaballa@users.noreply.github.com>
2023-01-19 22:33:13 +0200

    tool bar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Konva from 'konva';
+import { Layer } from 'konva/lib/Layer';
+import { Stage } from 'konva/lib/Stage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  stage!: Stage;
+  layer!: Layer;
   title = 'frontend';
+  ngOnInit(): void {
+    this.stage = new Stage({
+      container: "container",
+      width: window.innerWidth,
+      height:  window.innerHeight
+    });
+    this.layer = new Layer();
+    this.stage.add(this.layer);
+  }
+
+  drawCircle() {
+    
+    this.stage.on("mousedown",() => {
+      var circle = new Konva.Circle({
+        x: this.stage?.getRelativePointerPosition()?.x,
+        y: this.stage?.getRelativePointerPosition()?.y,
+        radius: 50,
+        fill: 'white',
+        stroke: 'black',
+        strokeWidth: 4,
+        draggable:true,
+      })
+    
+      this.layer.add(circle);
+      this.stage.add(this.layer);
+    });
+    
+  }
+  
+  drawRectangle() {///////////////////////////////\
+    this.stage.on("mousedown",() => {
+      var circle = new Konva.Rect({
+        x: this.stage?.getRelativePointerPosition()?.x,
+        y: this.stage?.getRelativePointerPosition()?.y,
+        width: 100,
+        height: 50,
+        fill: 'white',
+        stroke: 'black',
+        strokeWidth: 4,
+        draggable:true,
+      })
+      this.layer.add(circle);
+      this.stage.add(this.layer);
+    });
+}
+
+
 }
