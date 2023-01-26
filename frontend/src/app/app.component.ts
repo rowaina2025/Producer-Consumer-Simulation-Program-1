79faef649a02ed4413487802b762895d30b6f0d3
sara-gaballa <96298154+sara-gaballa@users.noreply.github.com>
2023-01-26 05:35:48 +0200

    line test

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -34,7 +34,69 @@ export class AppComponent implements OnInit{
       }
     });
   }
+  Connect(){
+    this.shape = 'arrow'
+    // this.stage.on("mousedown",(e) => {
+    //   if(e.target instanceof Konva.Circle) {/*select shape*/}
+    //   else if(this.shape == 'circle'){
+    //     this.drawShape('circle')
+    //     this.shape = ''
+    //     //TODO do your implementation on adding new machine
+    //   }
+    // });
 
+  let isdraw=false
+  let move=false
+  let arrow:any
+  let x:any,y:any
+  let pos:any
+    this.stage.on('mousedown', (e) => {
+      isdraw=true
+      if(this.shape == 'arrow') {
+        move = false
+        isdraw = true;
+        x = e.target.getRelativePointerPosition().x;
+        y = e.target.getRelativePointerPosition().y
+        // const pos = this.stage.getRelativePointerPosition();
+        arrow = new Konva.Arrow({
+          x: this.stage?.getRelativePointerPosition()?.x,
+          y: this.stage?.getRelativePointerPosition()?.y,
+          points: [x,y],
+          pointerLength: 20,
+          pointerWidth: 20,
+          fill: 'black',
+          stroke: 'black',
+          strokeWidth: 4,
+        });
+        this.layer.add(arrow).batchDraw();
+      }
+    });
+
+    this.stage.on('mousemove', () => {
+      if(this.shape == 'arrow') {
+        move = true;
+
+       pos = this.stage?.getRelativePointerPosition();
+      arrow.setAttrs({
+        points: [x, y, pos?.x, pos?.y],
+      })
+  }});
+
+    this.stage.on('mouseup', (e) => {
+      if(this.shape == 'arrow') {
+        isdraw = false;
+        if(move) {
+          
+          arrow.setAttrs({
+            points: [x, y, pos?.x, pos?.y],
+          })
+          this.layer.add(arrow).batchDraw();
+    
+          this.stage.add(this.layer);
+          arrow=null
+      }}
+  });
+  }
   addProducer() {
     this.shape = 'rect'
     this.stage.on("mousedown",(e) => {
@@ -74,5 +136,19 @@ export class AppComponent implements OnInit{
       this.layer.add(rect);
       this.stage.add(this.layer);
     }
+    // else if(shape == 'arrow') {
+    //   let arrow = new Konva.Arrow({
+    //     x: this.stage?.getRelativePointerPosition()?.x,
+    //     y: this.stage?.getRelativePointerPosition()?.y,
+    //     points: [0, 0, width / 2, height / 2],
+    //     pointerLength: 20,
+    //     pointerWidth: 20,
+    //     fill: 'black',
+    //     stroke: 'black',
+    //     strokeWidth: 4,
+    //   });
+    //   this.layer.add(arrow);
+    //   this.stage.add(this.layer);
+    // }
   }
 }
